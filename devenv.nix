afb1a4ea0029aa9af5ca6afb88ea4746d5d570ec
Ryan C. <ryan.chung.cs@gmail.com>
2025-07-10 21:04:47 -0400

    build(devenv): scaffold python environment

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,32 @@
 { pkgs, lib, config, inputs, ... }:
+
 {
   languages = {
+    shell.enable = true;
     nix.enable = true;
+    python = {
+      enable = true;
+	    uv.enable = true;
+	    venv.enable = true;
+    };
   };
+
   packages = [
     pkgs.zsh
     pkgs.git
+    pkgs.curl
+    pkgs.wget
+    pkgs.gcc12
+    pkgs.numactl
   ];
   enterShell = ''
     git --version
 
+    uv venv --python 3.12 --seed;
+    source .devenv/state/venv/bin/activate;
+
     # uvx --python 3.11 open-webui@latest serve
+  '';
 
   enterTest = ''
     echo "Running tests"
@@ -23,6 +39,6 @@
 
 
   env = {
-    DATA_DIR = "~/.open-webui";
+    DATA_DIR = ".open-webui";
   };
 }
